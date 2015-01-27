90d11588a6276f7d80c1ad0db4a51af3a46d4a83
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-01-27 11:09:54 +0100

    Issue #21: Added unit test example.

---
+++ modules/plug_example/src/Plugin/name/AcmeTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\plug_example\Plugin\name\AcmeTest
+ */
+
+namespace Drupal\plug_example\Plugin\name;
+
+/**
+ * Class Acme
+ * @package Drupal\plug_example\Plugin\name
+ *
+ * @Name(
+ *   id = "acme_test",
+ *   company = TRUE
+ * )
+ */
+class AcmeTest extends Acme {
+
+  /**
+   * Overrides NameBase::t().
+   *
+   * Overrides the translation method to avoid bootstrap.
+   */
+  protected function t($string, array $args = array(), array $options = array()) {
+    return $string;
+  }
+
+}
---
+++ modules/plug_example/src/Plugin/name/JohnTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\plug_example\Plugin\name\JohnTest
+ */
+
+namespace Drupal\plug_example\Plugin\name;
+
+/**
+ * Class John
+ * @package Drupal\plug_example\Plugin\name
+ *
+ * @Name(
+ *   id = "john_test",
+ *   company = FALSE
+ * )
+ */
+class JohnTest extends John {
+
+  /**
+   * Overrides NameBase::t().
+   *
+   * Overrides the translation method to avoid bootstrap.
+   */
+  protected function t($string, array $args = array(), array $options = array()) {
+    return $string;
+  }
+
+}
