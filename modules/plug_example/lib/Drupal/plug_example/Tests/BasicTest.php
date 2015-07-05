a145903867dd377198c991fc34957ac6f43bd12f
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-07-05 15:57:04 +0200

    :hamster:

--- modules/plug_example/lib/Drupal/plug_example/Tests/BasicTest.php
+++ modules/plug_example/lib/Drupal/plug_example/Tests/BasicTest.php
@@ -92,8 +92,8 @@ class BasicTest extends BaseTest {
     parent::setUp('registry_autoload', 'plug_example');
     // Get a new Name plugin manager to instantiate the test plugins.
     $this->manager = NamePluginManager::create();
-    print_r(db_query("SELECT * FROM {registry}")->fetchAllAssoc('name'), TRUE);
-    print_r(db_query("SELECT filename FROM {registry_file}")->fetchCol(), TRUE);
+    print_r(db_query("SELECT * FROM {registry}")->fetchAllAssoc('name'));
+    print_r(db_query("SELECT filename FROM {registry_file}")->fetchCol());
   }
 
   /**
